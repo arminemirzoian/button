ac3d649929f95d1bfb5c41b14a6899b9526d3b8c
arminemirzoian <arminemrzoian@gmail.com>
2022-02-23 14:00:00 +0400

    button first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import ButtonReact from "./components/Button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <ButtonReact
+                type="button"
+                boxSize="btn--box--middle"
+                buttonStyle="btn--primary--solid"
+                buttonSize="btn--middle"
+                onClick={onclick}
+                disabled={false}
+            >BUTTON</ButtonReact>
+        </div>
+    );
 }
 
 export default App;
